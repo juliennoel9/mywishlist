485993e974a7b01b3ec917a5d5f164276365b077
Julien NOEL <julien.noel5@etu.univ-lorraine.fr>
2019-12-03 08:54:22 +0100

    Ajout des fichiers de configuration ainsi que le Database.php pour la connexion

---
+++ src/config/Database.php
@@ -0,0 +1,28 @@
+<?php
+namespace mywishlist\config;
+use Illuminate\Database\Capsule\Manager as DB;
+
+class Database {
+
+    public static function connect(){
+        if(file_exists('src/config/database.ini')) {
+            $data = parse_ini_file('src/config/database.ini');
+        } else {
+            throw new \Exception("Fichier database.ini introuvable dans /src/config/");
+        }
+
+        $db = new DB();
+        $db->addConnection( [
+            'driver'    => $data['driver'],
+            'host'      => $data['host'],
+            'database'  => $data['database'],
+            'username'  => $data['username'],
+            'password'  => $data['password'],
+            'charset'   => $data['charset'],
+            'collation' => $data['collation'],
+            'prefix'    => ''
+        ]);
+        $db->setAsGlobal();
+        $db->bootEloquent();
+    }
+}
