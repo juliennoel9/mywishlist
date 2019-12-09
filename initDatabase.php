b196609c3f0f9fc25cfd5af3e6f3f5bb9987d29d
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-12-09 23:47:39 +0100

    ajout d'un script pour initialiser la BDD

---
+++ initDatabase.php
@@ -0,0 +1,84 @@
+<?php
+$stderr = fopen('php://stderr', 'w');
+$force = false;
+if (isset($argv[1]) && $argv[1] == '-f') { // option pour forcer l'exécution
+    $force = true;
+}
+
+
+/* lecture du fichier .ini */
+if (file_exists('src/config/database.ini')) {
+    $conf = parse_ini_file('src/config/database.ini');
+} else {
+    fwrite($stderr, "Fichier 'database.ini' introuvable dans /src/config/\n");
+    exit(1);
+}
+
+$driver = $conf['driver'];
+$host = $conf['host'];
+$database = $conf['database'];
+$username = $conf['username'];
+$password = $conf['password'];
+$charset = $conf['charset'];
+$collation = $conf['collation'];
+
+
+/* création de la BDD */
+try {
+    $conn = new PDO("$driver:host=$host", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // TODO set charset & collation from .ini
+    $stmt = $conn->prepare("SHOW DATABASES LIKE '$database'");
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_NUM);
+
+    if ($result) { // si la BDD existe deja
+        echo "Attention la BDD '$database' existe déjà, continuer entrainera la suppression des données de '$database'.\n";
+        if (!$force) {
+            $line = readline("Entrez 'ok' pour continuer: ");
+            if ($line != 'ok') {
+                echo "Annulation\n";
+                exit(2);
+            }
+        }
+        $conn->exec("DROP DATABASE $database");
+    }
+    $conn->exec("CREATE DATABASE IF NOT EXISTS $database");
+    $conn->exec("USE $database");
+    echo "La BDD '$database' à bien été initialisé.\n";
+} catch (PDOException $e) {
+    fwrite($stderr, $e->getMessage() . "\n");
+    exit(3);
+}
+
+
+/* création des tables */
+$sql = file_get_contents('src/config/mywishlist_create.sql');
+try {
+    $conn->exec($sql);
+    echo "Les tables ont bien été créées.\n";
+} catch (PDOException $e) {
+    fwrite($stderr, $e->getMessage() . "\n");
+    exit(4);
+}
+
+
+/* insertion des données */
+if (!$force) {
+    $line = strtolower(readline("Insérer les données d'exemple ? (o/n): "));
+}
+if ($force || $line == 'o' || $line == 'oui' || $line == 'y' || $line == 'yes') {
+    $sql = file_get_contents('src/config/mywishlist_insert.sql');
+    try {
+        $conn->exec($sql);
+        echo "Les données ont bien été insérées.\n";
+    } catch (PDOException $e) {
+        fwrite($stderr, $e->getMessage() . "\n");
+        exit(5);
+    }
+}
+
+$conn = null;
+echo "Terminé\n";
+exit(0);
+?>
---
+++ src/config/mywishlist_create.sql
@@ -0,0 +1,27 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `liste_id` int(11) NOT NULL,
+  `nom` text NOT NULL,
+  `descr` text,
+  `img` text,
+  `url` text,
+  `tarif` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `liste`;
+CREATE TABLE `liste` (
+  `no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `titre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `expiration` date DEFAULT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
