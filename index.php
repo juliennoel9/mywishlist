f8f2b9fb9629e790f5f81512331e4d829a442a6c
Julien NOEL <juliennoel9@gmail.com>
2019-12-10 17:34:36 +0100

    Ajout d'une route pour la Home Page avec Slim et implémentation de Php-View

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+use mywishlist\controllers\HomeController;
+use Slim\Views\PhpRenderer;
+
 require_once 'vendor/autoload.php';
 
 session_start();
@@ -7,11 +10,38 @@ session_start();
 \mywishlist\config\Database::connect();
 
 /**
+ * Dev. mode to show errors in details
+ */
+$config = [
+    'settings' => [
+        'displayErrorDetails' => 1,
+    ],
+];
+
+/**
  * Instanciation of Slim
  */
-$app = new Slim\App();
+$app = new Slim\App($config);
+$container = $app->getContainer();
 
+$container['view'] = function($container) {
+    $vars = [
+        "rootUri" => $container->request->getUri()->getBasePath(),
+        "title" => "MyWishList"
+    ];
+    $renderer = new PhpRenderer(__DIR__.'/src/views', $vars);
+    $renderer->setLayout('layout.html');
+    return $renderer;
+};
 
+/**
+ * Routes
+ */
+$app->get('/', function ($request, $response, array $args) {
+    global $container;
+    $controller = new HomeController($container);
+    return $controller->displayHome($request, $response, $args);
+})->setName('home');
 
 /**
  * Run of Slim
