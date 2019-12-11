5a85b6174ad2f4d282524e03887deefc61d050b4
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-12-11 22:52:17 +0100

    Fix readline() for all terminal

--- initDatabase.php
+++ initDatabase.php
@@ -1,16 +1,14 @@
 <?php
-$stderr = fopen('php://stderr', 'w');
 $force = false;
 if (isset($argv[1]) && $argv[1] == '-f') { // option pour forcer l'exécution
     $force = true;
 }
 
-
 /* lecture du fichier .ini */
 if (file_exists('src/config/database.ini')) {
     $conf = parse_ini_file('src/config/database.ini');
 } else {
-    fwrite($stderr, "Fichier 'database.ini' introuvable dans /src/config/\n");
+    fwrite(STDERR, "Fichier 'database.ini' introuvable dans /src/config/\n");
     exit(1);
 }
 
@@ -33,10 +31,10 @@ try {
     $result = $stmt->fetch(PDO::FETCH_NUM);
 
     if ($result) { // si la BDD existe deja
-        echo "Attention la BDD '$database' existe déjà, continuer entrainera la suppression des données de '$database'.\n";
+        echo "Attention la BDD '$database' existe déjà, continuer entrainera la suppression des données de cette BDD.\n";
         if (!$force) {
             echo "Entrez 'ok' pour continuer: ";
-            $line = readline();
+            $line = substr(stream_get_line(STDIN, 1024, "\n"),0,2);
             if ($line != 'ok') {
                 echo "Annulation\n";
                 exit(2);
@@ -48,18 +46,18 @@ try {
     $conn->exec("USE $database");
     echo "La BDD '$database' à bien été initialisé.\n";
 } catch (PDOException $e) {
-    fwrite($stderr, $e->getMessage() . "\n");
+    fwrite(STDERR, $e->getMessage() . "\n");
     exit(3);
 }
 
 
 /* création des tables */
-$sql = file_get_contents('src/config/mywishlist_create.sql');
+$sql = file_get_contents('src/config/database_create.sql');
 try {
     $conn->exec($sql);
     echo "Les tables ont bien été créées.\n";
 } catch (PDOException $e) {
-    fwrite($stderr, $e->getMessage() . "\n");
+    fwrite(STDERR, $e->getMessage() . "\n");
     exit(4);
 }
 
@@ -67,15 +65,15 @@ try {
 /* insertion des données */
 if (!$force) {
     echo "Insérer les données d'exemple ? (o/n): ";
-    $line = strtolower(readline());
+    $line = substr(stream_get_line(STDIN, 1024, "\n"),0,1);
 }
-if ($force || $line == 'o' || $line == 'oui' || $line == 'y' || $line == 'yes') {
-    $sql = file_get_contents('src/config/mywishlist_insert.sql');
+if ($force || $line == 'o' || $line == 'y') {
+    $sql = file_get_contents('src/config/database_insert.sql');
     try {
         $conn->exec($sql);
         echo "Les données ont bien été insérées.\n";
     } catch (PDOException $e) {
-        fwrite($stderr, $e->getMessage() . "\n");
+        fwrite(STDERR, $e->getMessage() . "\n");
         exit(5);
     }
 }
