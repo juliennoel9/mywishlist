e85d391e4a5c915fbc4e4d2c3c9c5c3fa529fc50
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-12-13 08:40:08 +0100

    Update DB creation script

--- src/config/database_create.sql
+++ src/config/database_create.sql
@@ -5,24 +5,24 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `liste_id` int(11) NOT NULL,
-  `nom` text NOT NULL,
-  `descr` text,
-  `img` text,
-  `url` text,
-  `tarif` decimal(5,2) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `liste_id` INT(11) NOT NULL,
+  `nom` TEXT NOT NULL,
+  `descr` TEXT,
+  `img` TEXT,
+  `url` TEXT,
+  `tarif` DECIMAL(5,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `liste`;
 CREATE TABLE `liste` (
-  `num` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `titre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci,
-  `expiration` TIMESTAMP DEFAULT NULL,
-  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `num` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) DEFAULT NULL,
+  `titre` VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` TEXT COLLATE utf8_unicode_ci,
+  `expiration` TIMESTAMP NOT NULL,
+  `token` VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `public` BOOLEAN DEFAULT FALSE,
   PRIMARY KEY (`num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
