c28c95d285a14db2208a9a4adf7b2d607465fdf7
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-12-13 10:27:07 +0100

    Amélioration du formulaire

--- src/controllers/ListController.php
+++ src/controllers/ListController.php
@@ -41,16 +41,14 @@ class ListController extends Controller {
 
     public function postNewList($request, $response, $args) {
         $list = new Liste();
-        $list->user_id = $_POST['idUser'];
-        $list->titre = $_POST['titre'];
-        $list->description = $_POST['description'];
-        $list->expiration = $_POST['expiration'];
+        $list->user_id = null;
+        $list->titre = htmlentities($_POST['titre']);
+        $list->description = htmlentities($_POST['description']);
+        $list->expiration = htmlentities($_POST['expiration']);
         $list->token = Liste::generateToken();
-        $list->public = 1;
+        $list->public = isset($_POST['public']) ? 1 : 0;
         $list->save();
 
-        //$this->view->render($response, 'home.phtml', ["title" => "MyWishList - Accueil"]);
-        //return $response;
         return $this->redirect($response, 'home');
     }
 }
