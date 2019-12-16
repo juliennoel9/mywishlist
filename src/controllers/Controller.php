f0e52a90e206b16990d8579cadf726f56fa328f7
Julien NOEL <julien.noel5@etu.univ-lorraine.fr>
2019-12-16 16:29:27 +0100

    Update redirect method in Controller

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -11,7 +11,11 @@ class Controller {
         $this->container = $container;
     }
 
-    public function redirect($response, $name, array $args) {
-        return $response->withStatus(302)->withHeader('Location', $this->container->router->pathFor($name, $args).$args['endPath']);
+    public function redirect($response, $name, array $args = []) {
+        if (isset($args['endPath'])){
+            return $response->withStatus(302)->withHeader('Location', $this->container->router->pathFor($name, $args).$args['endPath']);
+        }else {
+            return $response->withStatus(302)->withHeader('Location', $this->container->router->pathFor($name, $args));
+        }
     }
 }
