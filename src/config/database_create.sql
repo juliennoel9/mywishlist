deec1fe4ad0ac42e6f2a5f96183e8811f4db18ca
Louis Demange <48560751+Nekzuris@users.noreply.github.com>
2020-01-09 15:34:31 +0100

    Update Database (cagnotte)

--- src/config/database_create.sql
+++ src/config/database_create.sql
@@ -1,6 +1,14 @@
-SET NAMES utf8;
-SET foreign_key_checks = 0;
+SET NAMES utf8mb4;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+
+DROP TABLE IF EXISTS `cagnotte`;
+CREATE TABLE `cagnotte` (
+`item_id` INT(11) NOT NULL,
+`account_id` INT(11) NOT NULL,
+`tarif` DECIMAL(10,2) NOT NULL,
+PRIMARY KEY (`item_id`, `account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item` (
@@ -10,21 +18,31 @@ CREATE TABLE `item` (
   `descr` TEXT,
   `img` TEXT,
   `url` TEXT,
-  `tarif` DECIMAL(10,2) DEFAULT NULL,
-  `account_id` INT(11) DEFAULT NULL,
-  `nomReservation` VARCHAR(30) DEFAULT NULL,
+  `tarif` DECIMAL(10,2),
+  `account_id_reserv` INT(11),
   `messageReservation` TEXT,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+`id` INT(11) NOT NULL AUTO_INCREMENT,
+`liste_id` INT(11) NOT NULL,
+`account_id` INT(11),
+`nomMessage` VARCHAR(61) NOT NULL,
+`message` TEXT NOT NULL,
+`date` DATETIME NOT NULL,
+PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `liste`;
 CREATE TABLE `liste` (
   `num` INT(11) NOT NULL AUTO_INCREMENT,
-  `user_id` INT(11) DEFAULT NULL,
+  `user_id` INT(11) NOT NULL,
   `titre` VARCHAR(255) NOT NULL,
   `description` TEXT,
   `expiration` TIMESTAMP NOT NULL,
-  `token` VARCHAR(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `token` VARCHAR(255) COLLATE utf8mb4_bin NOT NULL,
   `public` BOOLEAN DEFAULT FALSE,
   PRIMARY KEY (`num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -40,23 +58,18 @@ CREATE TABLE `account` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE `message` (
-`id` INT(11) NOT NULL AUTO_INCREMENT,
-`liste_id` INT(11) NOT NULL,
-`account_id` INT(11) DEFAULT NULL,
-`nomMessage` VARCHAR(30),
-`message` TEXT,
-`date` datetime NOT NULL,
-PRIMARY KEY (`id`),
-FOREIGN KEY (`liste_id`) REFERENCES `liste`(num),
-FOREIGN KEY (`account_id`) REFERENCES `account`(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*
+
+ALTER TABLE `cagnotte`
+ADD FOREIGN KEY (`item_id`) REFERENCES `item`(id) ON DELETE CASCADE,
+ADD FOREIGN KEY (`account_id`) REFERENCES `account`(id) ON DELETE CASCADE;
+
 ALTER TABLE `item`
 ADD FOREIGN KEY (`liste_id`) REFERENCES `liste`(num) ON DELETE CASCADE,
-ADD FOREIGN KEY (`account_id`) REFERENCES `account`(id) ON DELETE CASCADE;
-  
+ADD FOREIGN KEY (`account_id_reserv`) REFERENCES `account`(id) ON DELETE SET NULL;
+
+ALTER TABLE `message`
+ADD FOREIGN KEY (`liste_id`) REFERENCES `liste`(num) ON DELETE CASCADE,
+ADD FOREIGN KEY (`account_id`) REFERENCES `account`(id) ON DELETE SET NULL;
+
 ALTER TABLE `liste`
-ADD FOREIGN KEY (`user_id`) REFERENCES `account`(id);
-*/
+ADD FOREIGN KEY (`user_id`) REFERENCES `account`(id) ON DELETE CASCADE;
--- src/config/database_insert.sql
+++ src/config/database_insert.sql
@@ -1,27 +1,28 @@
-INSERT INTO `item` (`id`, `liste_id`, `nom`, `descr`, `img`, `url`, `tarif`) VALUES
-(1, 2, 'Champagne', 'Bouteille de champagne + flutes + jeux à gratter', 'champagne.jpg', '', 20.00),
-(2, 2, 'Musique', 'Partitions de piano à 4 mains', 'musique.jpg', '', 25.00),
-(3, 2, 'Exposition', 'Visite guidée de l’exposition ‘REGARDER’ à la galerie Poirel', 'poirelregarder.jpg', '', 14.00),
-(4, 3, 'Goûter', 'Goûter au FIFNL', 'gouter.jpg', '', 20.00),
-(5, 3, 'Projection', 'Projection courts-métrages au FIFNL', 'film.jpg', '', 10.00),
-(6, 2, 'Bouquet', 'Bouquet de roses et Mots de Marion Renaud', 'rose.jpg', '', 16.00),
-(7, 2, 'Diner Stanislas', 'Diner à La Table du Bon Roi Stanislas (Apéritif /Entrée / Plat / Vin / Dessert / Café / Digestif)', 'bonroi.jpg', '', 60.00),
-(8, 3, 'Origami', 'Baguettes magiques en Origami en buvant un thé', 'origami.jpg', '', 12.00),
-(9, 3, 'Livres', 'Livre bricolage avec petits-enfants + Roman', 'bricolage.jpg', '', 24.00),
-(10, 2, 'Diner  Grand Rue ', 'Diner au Grand’Ru(e) (Apéritif / Entrée / Plat / Vin / Dessert / Café)', 'grandrue.jpg', '', 59.00),
-(11, 0, 'Visite guidée', 'Visite guidée personnalisée de Saint-Epvre jusqu’à Stanislas', 'place.jpg', '', 11.00),
-(12, 2, 'Bijoux', 'Bijoux de manteau + Sous-verre pochette de disque + Lait après-soleil', 'bijoux.jpg', '', 29.00),
-(19, 0, 'Jeu contacts', 'Jeu pour échange de contacts', 'contact.png', '', 5.00),
-(22, 0, 'Concert', 'Un concert à Nancy', 'concert.jpg', '', 17.00),
-(23, 1, 'Appart Hotel', 'Appart’hôtel Coeur de Ville, en plein centre-ville', 'apparthotel.jpg', '', 56.00),
-(24, 2, 'Hôtel d\'Haussonville', 'Hôtel d\'Haussonville, au coeur de la Vieille ville à deux pas de la place Stanislas', 'hotel_haussonville_logo.jpg', '', 169.00),
-(25, 1, 'Boite de nuit', 'Discothèque, Boîte tendance avec des soirées à thème & DJ invités', 'boitedenuit.jpg', '', 32.00),
-(26, 1, 'Planètes Laser', 'Laser game : Gilet électronique et pistolet laser comme matériel, vous voilà équipé.', 'laser.jpg', '', 15.00),
-(27, 1, 'Fort Aventure', 'Découvrez Fort Aventure à Bainville-sur-Madon, un site Accropierre unique en Lorraine ! Des Parcours Acrobatiques pour petits et grands, Jeu Mission Aventure, Crypte de Crapahute, Tyrolienne, Saut à l\'élastique inversé, Toboggan géant... et bien plus encore.', 'fort.jpg', '', 25.00);
+INSERT INTO `account` (`username`, `email`, `hash`, `nom`, `prenom`) VALUES
+('root', 'root@root.com', '$2y$10$zUnLFNxa0iP4svm5PMKvHu8u7Z8LWsedo0udjxQqfcJJa8h1CKRE2', 'root', 'root');
 
-INSERT INTO `liste` (`num`, `user_id`, `titre`, `description`, `expiration`, `token`, `public`) VALUES
-(1, 1, 'Pour fêter le bac !', 'Pour un week-end à Nancy qui nous fera oublier les épreuves. ', '2020-06-27', 'HOf3UXyX', TRUE),
-(2, 1, 'Liste de mariage d\'Alice et Bob', 'Nous souhaitons passer un week-end royal à Nancy pour notre lune de miel :)', '2018-06-30', '54xs1en0', TRUE),
-(3, 1, 'C\'est l\'anniversaire de Charlie', 'Pour lui préparer une fête dont il se souviendra :)', '2017-12-12', 'seab3twa', FALSE);
+INSERT INTO `liste` (`user_id`, `titre`, `description`, `expiration`, `token`, `public`) VALUES
+(1, 'Pour fêter le bac !', 'Pour un week-end à Nancy qui nous fera oublier les épreuves. ', '2020-06-27', 'HOf3UXyX', TRUE),
+(1, 'Liste de mariage d\'Alice et Bob', 'Nous souhaitons passer un week-end royal à Nancy pour notre lune de miel :)', '2018-06-30', '54xs1en0', TRUE),
+(1, 'C\'est l\'anniversaire de Charlie', 'Pour lui préparer une fête dont il se souviendra :)', '2017-12-12', 'seab3twa', FALSE);
 
-INSERT INTO `account` (`id`, `username`, `email`, `hash`, `nom`, `prenom`) VALUES (1, 'root', 'root@root.com', '$2y$10$zUnLFNxa0iP4svm5PMKvHu8u7Z8LWsedo0udjxQqfcJJa8h1CKRE2', 'root', 'root');
+INSERT INTO `item` (`liste_id`, `nom`, `descr`, `img`, `url`, `tarif`) VALUES
+(1, 'Appart Hotel', 'Appart’hôtel Coeur de Ville, en plein centre-ville', 'apparthotel.jpg', '', 56.00),
+(1, 'Boite de nuit', 'Discothèque, Boîte tendance avec des soirées à thème & DJ invités', 'boitedenuit.jpg', '', 32.00),
+(1, 'Planètes Laser', 'Laser game : Gilet électronique et pistolet laser comme matériel, vous voilà équipé.', 'laser.jpg', '', 15.00),
+(1, 'Fort Aventure', 'Découvrez Fort Aventure à Bainville-sur-Madon, un site Accropierre unique en Lorraine ! Des Parcours Acrobatiques pour petits et grands, Jeu Mission Aventure, Crypte de Crapahute, Tyrolienne, Saut à l\'élastique inversé, Toboggan géant... et bien plus encore.', 'fort.jpg', '', 25.00),
+(2, 'Champagne', 'Bouteille de champagne + flutes + jeux à gratter', 'champagne.jpg', '', 20.00),
+(2, 'Musique', 'Partitions de piano à 4 mains', 'musique.jpg', '', 25.00),
+(2, 'Exposition', 'Visite guidée de l’exposition ‘REGARDER’ à la galerie Poirel', 'poirelregarder.jpg', '', 14.00),
+(2, 'Bouquet', 'Bouquet de roses et Mots de Marion Renaud', 'rose.jpg', '', 16.00),
+(2, 'Diner Stanislas', 'Diner à La Table du Bon Roi Stanislas (Apéritif /Entrée / Plat / Vin / Dessert / Café / Digestif)', 'bonroi.jpg', '', 60.00),
+(2, 'Diner  Grand Rue ', 'Diner au Grand’Ru(e) (Apéritif / Entrée / Plat / Vin / Dessert / Café)', 'grandrue.jpg', '', 59.00),
+(2, 'Visite guidée', 'Visite guidée personnalisée de Saint-Epvre jusqu’à Stanislas', 'place.jpg', '', 11.00),
+(2, 'Bijoux', 'Bijoux de manteau + Sous-verre pochette de disque + Lait après-soleil', 'bijoux.jpg', '', 29.00),
+(2, 'Hôtel d\'Haussonville', 'Hôtel d\'Haussonville, au coeur de la Vieille ville à deux pas de la place Stanislas', 'hotel_haussonville_logo.jpg', '', 169.00),
+(3, 'Goûter', 'Goûter au FIFNL', 'gouter.jpg', '', 20.00),
+(3, 'Projection', 'Projection courts-métrages au FIFNL', 'film.jpg', '', 10.00),
+(3, 'Origami', 'Baguettes magiques en Origami en buvant un thé', 'origami.jpg', '', 12.00),
+(3, 'Livres', 'Livre bricolage avec petits-enfants + Roman', 'bricolage.jpg', '', 24.00),
+(3, 'Jeu contacts', 'Jeu pour échange de contacts', 'contact.png', '', 5.00),
+(3, 'Concert', 'Un concert à Nancy', 'concert.jpg', '', 17.00);
