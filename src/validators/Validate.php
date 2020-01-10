f0b44a58b92bc5af9ca69df3e43b9aa93475b711
Louis Demange <48560751+Nekzuris@users.noreply.github.com>
2020-01-10 02:21:43 +0100

    Validate first and last name

--- src/validators/Validate.php
+++ src/validators/Validate.php
@@ -8,16 +8,53 @@ use Slim\Http\Response;
 class Validate {
 
     public static function username(string $input) : array {
-        if (!self::matchRegex('/[a-zA-Z0-9_-]{1,20}/', $input)) {
+        if (!self::matchRegex('/[a-zA-Z0-9_-]+/', $input)) {
             return ['valid' => false, 'error' => 'Utilisez uniquement des lettres (sans accents), chiffres, tirets et underscores.'];
         }
-        if (strlen($input) < 3) {
+        $l = strlen($input);
+        if ($l < 3) {
             return ['valid' => false, 'error' => "Votre nom d'utilisateur doit faire entre 3 et 20 caractères."];
+        }
+        if ($l > 20) {
+            return ['valid' => false, 'error' => "Votre nom d'utilisateur doit faire entre 3 et 20 caractères (vous utilisez $l caractères)."];
         }
         if (Account::select('username')->where('username', '=', $input)->first()) {
             return ['valid' => false, 'error' => "Ce nom d'utilisateur est déjà utilisé."];
         }
         return ['valid' => true, 'error' => ''];
+    }
+
+    public static function name(string $input, string $name) : array {
+        $allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáÁàÀâÂäÄãÃåÅæÆçÇéÉèÈêÊëËíÍìÌîÎïÏñÑóÓòÒôÔöÖõÕøØœŒßúÚùÙûÛüÜ-';
+        $illegal_ = [];
+        foreach (str_split($input) as $char) {
+            if (!strpbrk($char, $allowed) && !in_array($char, $illegal_)) {
+                array_push($illegal_, $char);
+            }
+        }
+        $illegal = implode('', $illegal_);
+        if (count($illegal_) === 1) {
+            return ['valid' => false, 'error' => "Le caractère \"$illegal\" n'est pas autorisé."];
+        } elseif (count($illegal_) > 1) {
+            return ['valid' => false, 'error' => "Les caractères \"$illegal\" ne sont pas autorisés."];
+        }
+        // convert multi byte encoded char to '?' for right counting ( strlen('é') = 2 )
+        $l = strlen(utf8_decode($input));
+        if ($l < 2) {
+            return ['valid' => false, 'error' => "Votre $name doit faire entre 2 et 30 caractères."];
+        }
+        if ($l > 30) {
+            return ['valid' => false, 'error' => "Votre $name doit faire entre 2 et 30 caractères (vous utilisez $l caractères)."];
+        }
+        return ['valid' => true, 'error' => ''];
+    }
+
+    public static function firstName(string $input) : array {
+        return self::name($input, 'prénom');
+    }
+
+    public static function lastName(string $input) : array {
+        return self::name($input, 'nom');
     }
 
     public static function email(string $input) : array {
@@ -55,9 +92,9 @@ class Validate {
             return $response->write(json_encode(['valid' => false, 'error' => 'Incorrect API use.']));
         }
         if (!method_exists(Validate::class, $type)) {
-            return $response->write(json_encode(['valid' => false, 'error' => "Method '$type' doesn't exist."]));
+            return $response->write(json_encode(['valid' => false, 'error' => "Method $type() doesn't exist."]));
         }
-        $input = isset($_GET[$type]) ? trim($_GET[$type]) : '';
+        $input = isset($_GET[$type]) ? trim(urldecode($_GET[$type])) : '';
         $response->write(json_encode(Validate::$type($input)));
         return $response;
     }
